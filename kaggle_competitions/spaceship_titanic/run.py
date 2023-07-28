075268eed788ccd4803b7d73fcee14161c2702a7
alekseik1 <1alekseik1@gmail.com>
2023-07-28 11:45:44 +0300

    refactor(sp-titanic): structure

--- kaggle_competitions/spaceship_titanic/run.py
+++ kaggle_competitions/spaceship_titanic/run.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 from sklearn.dummy import DummyClassifier
 from sklearn.metrics import accuracy_score, log_loss, precision_score, recall_score
@@ -12,18 +13,21 @@ target_col = "Transported"
 
 
 def load_train(dir_: Path = basedir) -> pd.DataFrame:
-    return pd.read_csv(dir_ / "data" / "train.csv")
+    return pd.read_csv(dir_ / "data" / "train.csv", index_col=0)
 
 
 def load_test(dir_: Path = basedir) -> pd.DataFrame:
-    return pd.read_csv(dir_ / "data" / "test.csv")
+    return pd.read_csv(dir_ / "data" / "test.csv", index_col=0)
 
 
-def baseline_model() -> DummyClassifier:
-    return DummyClassifier(strategy="most_frequent", random_state=random_state)
+def run_baseline(x: pd.DataFrame, y: pd.Series, test_df: pd.DataFrame) -> np.ndarray:
+    clf = DummyClassifier(strategy="most_frequent", random_state=random_state)
+    clf.fit(x, y)
+    log_metrics(x, y, clf, model_name="baseline")
+    return clf.predict(test_df)
 
 
-def log_metrics(x, y_true, estimator, model_name):
+def log_metrics(dir_: Path, x, y_true, estimator, model_name):
     for name, func in [
         ("accuracy", accuracy_score),
         ("precision", precision_score),
@@ -31,19 +35,19 @@ def log_metrics(x, y_true, estimator, model_name):
         ("log_loss", log_loss),
     ]:
         metrics.log_metrics(func(y_true, estimator.predict(x)), model_name, name)
-    metrics.save_metrics(basedir / "metrics.json")
+    metrics.save_metrics(dir_ / "metrics.json")
+
+
+def save_submit(dir_: Path, predictions: np.ndarray, test_df: pd.DataFrame):
+    pd.DataFrame({"Transported": predictions}, index=test_df.index).to_csv(dir_ / "data" / "submission.csv")
 
 
 def run():
     df_train, df_test = load_train(), load_test()
     x, y = df_train.drop(columns=[target_col]), df_train[target_col]
 
-    model = baseline_model()
-    model.fit(x, y)
-    log_metrics(x, y, model, model_name="baseline")
-    pd.DataFrame({"PassengerId": df_test["PassengerId"], "Transported": model.predict(df_test)}).to_csv(
-        basedir / "data" / "submission.csv", index=False
-    )
+    baseline_pred = run_baseline(x, y, df_test)
+    save_submit(basedir, baseline_pred, df_test)
 
 
 if __name__ == "__main__":
